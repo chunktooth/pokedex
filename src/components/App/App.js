2afcc7b37e4d12dfcae5a77803e09894dd3ea791
chunktooth <om.saetehn@gmail.com>
2018-04-12 09:22:03 -0600

    setting up files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,23 @@ import FakeContainer from '../../containers/FakeContainer/'
 
 class App extends Component {
 
+  getTypes = async (types) => {
+    try {
+      const response = await fetch('localhost:3001/types', {
+        method: 'GET',
+        body: JSON.stringify(types);
+        headers: {
+          'content-type': 'application/json';
+        }
+      });
+      console.log(response);
+      
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   render() {
     return (
       <div className='App'>
